3e04eb174e2223ca9740eead383de2abf80c771f
bilalsahmed007-sketch <bilal.s.ahmed007@gmail.com>
2026-02-27 19:13:22 +0500

    Create data.js

---
+++ data.js
@@ -0,0 +1,7 @@
+const retailerData = {
+    "369232": { "name": "Retailer 369232", "balance": "0", "status": "Active" },
+    "379638": { "name": "Retailer 379638", "balance": "0", "status": "Active" },
+    "369244": { "name": "Retailer 369244", "balance": "0", "status": "Active" },
+    "369241": { "name": "Retailer 369241", "balance": "0", "status": "Active" },
+    "369233": { "name": "Retailer 369233", "balance": "0", "status": "Active" }
+};
