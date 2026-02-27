9630ede986225eb9cba09fd4c61e02735845ea37
bilalsahmed007-sketch <bilal.s.ahmed007@gmail.com>
2026-02-27 19:40:12 +0500

    Update data.js

--- data.js
+++ data.js
@@ -1,11 +1,27 @@
 const retailerData = {
-    "369232": { "name": "Al-Makkah Store", "target": 1500, "balance": 450 },
-    "379638": { "name": "City Mart", "target": 2000, "balance": 1200 },
-    "369244": { "name": "Zubair Traders", "target": 1000, "balance": 300 },
-    "369241": { "name": "Bismillah General Store", "target": 1800, "balance": 900 },
-    "369233": { "name": "Green Grocery", "target": 2500, "balance": 2100 },
-    "379665": { "name": "Sky Traders", "target": 1200, "balance": 400 },
-    "379633": { "name": "Star Mart", "target": 3000, "balance": 1500 },
-    "372219": { "name": "Usman & Sons", "target": 900, "balance": 850 }
-    // Add the rest of your 1000 IDs here following this same pattern
+    "369232": { 
+        "name": "Al-Makkah Store", 
+        "target": 1500, 
+        "balance": 450,
+        "6mg": 100, "10mg": 50, "14mg": 200, "17mg": 100 
+    },
+    "379638": { 
+        "name": "City Mart", 
+        "target": 2000, 
+        "balance": 1200,
+        "6mg": 300, "10mg": 400, "14mg": 100, "17mg": 400 
+    },
+    "369244": { 
+        "name": "Zubair Traders", 
+        "target": 1000, 
+        "balance": 300,
+        "6mg": 50, "10mg": 50, "14mg": 100, "17mg": 100 
+    },
+    "369241": { 
+        "name": "Bismillah General Store", 
+        "target": 1800, 
+        "balance": 900,
+        "6mg": 200, "10mg": 200, "14mg": 250, "17mg": 250 
+    }
+    // Add new retailers above this line using the same format
 };
