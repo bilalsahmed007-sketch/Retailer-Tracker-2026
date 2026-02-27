bab18f2e72711d358d3ba2e04c4e3d984f0710c0
bilalsahmed007-sketch <bilal.s.ahmed007@gmail.com>
2026-02-27 21:28:12 +0500

    Update data.js

--- data.js
+++ data.js
@@ -1,46 +1,26 @@
-const retailerData = [
-    {
-        "Bizom ID": "369232",
-        "Shop Name": "Al-Makkah Store",
-        "Target": 1500,
-        "Total Sales": 450,
-        "6mg": 100,
-        "10mg": 50,
-        "14mg": 200,
-        "17mg": 100,
-        "Market": "Kasur"
+const retailerData = {
+    "369232": { 
+        "name": "Al-Makkah Store", 
+        "target": 1500, 
+        "balance": 450,
+        "6mg": 100, "10mg": 50, "14mg": 200, "17mg": 100 
     },
-    {
-        "Bizom ID": "379638",
-        "Shop Name": "City Mart",
-        "Target": 2000,
-        "Total Sales": 1200,
-        "6mg": 300,
-        "10mg": 400,
-        "14mg": 100,
-        "17mg": 400,
-        "Market": "Kasur"
+    "379638": { 
+        "name": "City Mart", 
+        "target": 2000, 
+        "balance": 1200,
+        "6mg": 300, "10mg": 400, "14mg": 100, "17mg": 400 
     },
-    {
-        "Bizom ID": "369244",
-        "Shop Name": "Zubair Traders",
-        "Target": 1000,
-        "Total Sales": 300,
-        "6mg": 50,
-        "10mg": 50,
-        "14mg": 100,
-        "17mg": 100,
-        "Market": "Kasur"
+    "369244": { 
+        "name": "Zubair Traders", 
+        "target": 1000, 
+        "balance": 300,
+        "6mg": 50, "10mg": 50, "14mg": 100, "17mg": 100 
     },
-    {
-        "Bizom ID": "369241",
-        "Shop Name": "Bismillah General Store",
-        "Target": 1800,
-        "Total Sales": 900,
-        "6mg": 200,
-        "10mg": 200,
-        "14mg": 250,
-        "17mg": 250,
-        "Market": "Kasur"
+    "369241": { 
+        "name": "Bismillah General Store", 
+        "target": 1800, 
+        "balance": 900,
+        "6mg": 200, "10mg": 200, "14mg": 250, "17mg": 250 
     }
-];
+};
